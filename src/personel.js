d01952356aa58827b6a17fb90d613f8ce970e601
zer0 <sahinonur2000@hotmail.com>
2019-07-30 15:25:31 +0300

    small fix

--- src/personel.js
+++ src/personel.js
@@ -1,8 +1,10 @@
 (() => {
     const adElement = document.getElementById('ad');
     adElement.onchange(_ => {
+        console.log('setting name to: ' + adElement.innerText);
         Cookies.set('ad', adElement.innerText);
     });
     let oldText = Cookies.get('ad');
+    console.log('oldText: ' + oldText);
     adElement.innerText = oldText;
 })()
