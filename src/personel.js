1790a0aa976ffb42cc4194cbf9c65c6205f49ef9
zer0 <sahinonur2000@hotmail.com>
2019-07-30 15:49:02 +0300

    refactoring

--- src/personel.js
+++ src/personel.js
@@ -1,4 +1,4 @@
-(() => {
+/* (() => {
     const adElement = document.getElementById('ad');
 	console.log('adElement: ' + adElement);
     adElement.onchange = (_) => {
@@ -8,4 +8,16 @@
     let oldText = Cookies.get('ad');
     console.log('oldText: ' + oldText);
     adElement.value = oldText;
+})() */
+(() => {
+    const connectCookieEvent = (id) => {
+        const element = document.getElementById(id);
+        element.onchange = _ =>  {
+            Cookies.set(id, element.value);
+        };
+        let oldText = Cookies.get(id);
+        element.value = oldText;
+        console.log('connected cookie event to: ' + id);
+    };
+    connectCookieEvent('ad');
 })()
