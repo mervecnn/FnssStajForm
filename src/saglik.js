d39d55ad200839ec0d47926aeb9869d0f5caca6c
zer0 <sahinonur2000@hotmail.com>
2019-07-31 08:45:53 +0300

    remembers the state of inputs

---
+++ src/saglik.js
@@ -0,0 +1,19 @@
+(() => {
+    const ids = Array.from(document.getElementById('inputs')
+        .getElementsByClassName('input-md'))
+        .map(x => x.id);
+    console.log("Found ids: " + ids);
+
+    const connectCookieEvent = (id) => {
+		console.log(id);	
+        const element = document.getElementById(id);
+        element.onchange = _ =>  {
+            Cookies.set(id, element.value);
+        };
+        let oldText = Cookies.get(id);
+		if (oldText)
+			element.value = oldText;
+        console.log('connected cookie event to: ' + id);
+    };
+	ids.forEach(connectCookieEvent);
+})()
